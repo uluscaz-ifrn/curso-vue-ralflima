4be2c7139e321b35a4a019da511d04fdc2d7f487
uluscaz-ifrn <dantas.lucas2@escolar.ifrn.edu.br>
2025-03-08 19:54:45 -0300

    botao para exibir texto

--- src/components/Condicional.vue
+++ src/components/Condicional.vue
@@ -1,6 +1,9 @@
 <script setup>
-let exibir = true;
+import {ref} from 'vue';
+const texto = ref('Texto de exemplo')
+const exibir = ref (true);
 </script>
 <template>
-    <h1 v-if="exibir">Hellow World!</h1>
+    <h1 v-if="exibir">{{ texto }}</h1>
+    <button @click="exibir = !exibir">Click</button>
 </template>
