6728b76ccc2ed2a561a6bc5a0c2501fa6c748c12
uluscaz-ifrn <dantas.lucas2@escolar.ifrn.edu.br>
2025-03-08 20:05:28 -0300

    feat: mais exemplos

--- src/components/Interpolacao.vue
+++ src/components/Interpolacao.vue
@@ -5,6 +5,8 @@ let num1 = 4
 let num2 = 3 
 </script>
 <template>
-  <p>{{'Olá ' + nome + ' voce tem ' + idade + ' anos.' }}</p>
-  <p>{{ num1+num2 }}</p>
+  <h1>Interpolação e Concatenação:</h1>
+  <p>{{'Olá ' + nome + ' você tem ' + idade + ' anos.' }}</p>
+  <h1>Soma dentro do próprio elemento:</h1>
+  <p>{{ 'A soma é: ' + (num1+num2) }}</p>
 </template>
