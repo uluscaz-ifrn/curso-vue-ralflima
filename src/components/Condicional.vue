d38a20d1b71ac1ba15a92c8676984f188cb0fea5
uluscaz-ifrn <dantas.lucas2@escolar.ifrn.edu.br>
2025-03-09 19:50:24 -0300

    feat: novos exemplos

--- src/components/Condicional.vue
+++ src/components/Condicional.vue
@@ -2,8 +2,12 @@
 import {ref} from 'vue';
 const texto = ref('Texto de exemplo')
 const exibir = ref (true);
+let media = 8
 </script>
 <template>
     <h1 v-if="exibir">{{ texto }}</h1>
     <button @click="exibir = !exibir">Click</button>
+    <p v-if="media >= 7">Aprovado(a)</p>
+    <p v-else-if="media >= 5">Em exame</p>
+    <p v-else>Reprovado(a)</p>
 </template>
