827db09a7dbcda02140f030de76b5f6869e355fb
uluscaz-ifrn <dantas.lucas2@escolar.ifrn.edu.br>
2025-03-09 20:05:52 -0300

    feat: exemplo v-html

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import Interpolacao from './components/Interpolacao.vue';
 import Condicional from './components/Condicional.vue';
 import Laco from './components/Laco.vue';
+import Vhtml from './components/Vhtml.vue';
 </script>
 <template>
   <Interpolacao></Interpolacao>
   <Condicional></Condicional>
   <Laco></Laco>
+  <Vhtml></Vhtml>
 </template>
---
+++ src/components/Vhtml.vue
@@ -0,0 +1,6 @@
+<script setup>
+let texto = "<p>Aprendendo a diretiva v-html</p>";
+</script>
+<template>
+    <div v-html="texto"></div>
+</template>
